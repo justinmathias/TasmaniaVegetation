c953f0685cfb7e31fb725fc2736242ccc5e35d0a
justinmathias <justin.m.mathias@gmail.com>
2023-03-12 13:57:53 +1100

    add url for tmax and tmin

--- RScripts/GetDayCentClimate.R
+++ RScripts/GetDayCentClimate.R
@@ -1,3 +1,5 @@
+#Created by Justin Mathias 3/12/
+
 library('easypackages')
 libraries(c('tidyverse','HelpersMG', 'ncdf4'))
 
@@ -7,8 +9,17 @@ tassites <- data.frame('SiteID' = c('TheKeepDry','TheKeepWet','HolwellDry','Holw
                        'Longitude' = c(148.07137, 148.06780, 146.77529, 146.76602, 146.42546, 146.58134, 146.25085, 145.61582, 145.12642, 145.01781))
 
 
-#The following is the base URL for 1900:
+#The following is the base URL for 2020 for precip total:
 #https://dapds00.nci.org.au/thredds/ncss/zv2/agcd/v1/precip/total/r005/01day/agcd_v1_precip_total_r005_daily_2020.nc?var=precip&north=-41.14242&west=148.04657&east=148.09657&south=-41.19242&horizStride=1&time_start=2020-01-01T09%3A00%3A00Z&time_end=2020-12-31T09%3A00%3A00Z&timeStride=100&addLatLon=true
+
+#The following is the base URL for 2020 for tmax mean:
+#https://dapds00.nci.org.au/thredds/ncss/zv2/agcd/v1/tmax/mean/r005/01day/agcd_v1_tmax_mean_r005_daily_2020.nc?var=tmax&north=-41.14242&west=148.04657&east=148.09657&south=-41.19242&horizStride=1&time_start=2020-01-01T09%3A00%3A00Z&time_end=2020-12-31T09%3A00%3A00Z&timeStride=100&addLatLon=true
+
+#The following is the base URL for 2020 for tmin mean:
+#https://dapds00.nci.org.au/thredds/ncss/zv2/agcd/v1/tmin/mean/r005/01day/agcd_v1_tmin_mean_r005_daily_2020.nc?var=tmin&north=-41.14242&west=148.04657&east=148.09657&south=-41.19242&horizStride=1&time_start=2020-01-01T09%3A00%3A00Z&time_end=2020-12-31T09%3A00%3A00Z&timeStride=100&addLatLon=true
+
+
+
 
 
 latnorth <- tassites$Latitude+0.01
