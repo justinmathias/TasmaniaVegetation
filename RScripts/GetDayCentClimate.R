95e221adc9661bee7a0f2fd29adc108e5ba438a6
justinmathias <justin.m.mathias@gmail.com>
2023-03-28 17:03:21 +1100

    update files

--- RScripts/GetDayCentClimate.R
+++ RScripts/GetDayCentClimate.R
@@ -1,4 +1,4 @@
-#Created by Justin Mathias 3/12/
+#Created by Justin Mathias 3/12/23
 
 library('easypackages')
 libraries(c('tidyverse','HelpersMG', 'ncdf4'))
@@ -16,14 +16,14 @@ tassites <- data.frame('SiteID' = c('TheKeepDry','TheKeepWet','HolwellDry','Holw
 #The following is the base URL for 2020 for tmin mean:
 #https://dapds00.nci.org.au/thredds/ncss/zv2/agcd/v1/tmin/mean/r005/01day/agcd_v1_tmin_mean_r005_daily_2020.nc?var=tmin&north=-41.14242&west=148.04657&east=148.09657&south=-41.19242&horizStride=1&time_start=2020-01-01T09%3A00%3A00Z&time_end=2020-12-31T09%3A00%3A00Z&timeStride=100&addLatLon=true
 
-#Define boundary points used to get NCDF files of climate data from thredds server--we need a rectangle as we cannot extract single points.
+#Define boundary points used to get NCDF files of climate data for each of our sites from thredds server--we need a rectangle as we cannot extract single points.
 latnorth <- tassites$Latitude+0.01
 latsouth <- tassites$Latitude-0.01
 lonwest <- tassites$Longitude+0.01
 loneast <- tassites$Longitude-0.01
 
 #Define years over which we want to extract climate data
-yrs <- 1900:2020
+yrs <- 1900:1901
 
 #Define URLs for precip data products (NCDF files) to download from thredds server
 precip.files <- vector("list", length(yrs)*length(latnorth)) #Create empty list to store filenames in 
@@ -32,12 +32,47 @@ for (i in 1:length(yrs)) {
   precip.files[[i]][j] <- paste0("https://dapds00.nci.org.au/thredds/ncss/zv2/agcd/v1/precip/total/r005/01day/agcd_v1_precip_total_r005_daily_",yrs[i],".nc?var=precip&north=",latnorth[j],"&west=",lonwest[j],"&east=",loneast[j],"&south=",latsouth[j],"&horizStride=1&time_start=",yrs[i],"-01-01T09%3A00%3A00Z&time_end=",yrs[i],"-12-31T09%3A00%3A00Z&timeStride=100&addLatLon=true")
   }
 }
-ppt <- unlist(precip.files)
-ppt
+ppt.urls <- unlist(precip.files)
+
+#Define URLs for tmax data products (NCDF files) to download from thredds server
+tmax.files <- vector("list", length(yrs)*length(latnorth)) #Create empty list to store filenames in 
+for (i in 1:length(yrs)) {
+  for (j in 1:length(tassites$SiteID)) {
+    tmax.files[[i]][j] <- paste0("https://dapds00.nci.org.au/thredds/ncss/zv2/agcd/v1/tmax/mean/r005/01day/agcd_v1_tmax_mean_r005_daily_",yrs[i],".nc?var=tmax&north=",latnorth[j],"&west=",lonwest[j],"&east=",loneast[j],"&south=",latsouth[j],"&horizStride=1&time_start=",yrs[i],"-01-01T09%3A00%3A00Z&time_end=",yrs[i],"-12-31T09%3A00%3A00Z&timeStride=100&addLatLon=true")
+  }
+}
+tmax.urls <- unlist(tmax.files)
+
+#Define URLs for tmin data products (NCDF files) to download from thredds server
+tmin.files <- vector("list", length(yrs)*length(latnorth)) #Create empty list to store filenames in 
+for (i in 1:length(yrs)) {
+  for (j in 1:length(tassites$SiteID)) {
+    tmin.files[[i]][j] <- paste0("https://dapds00.nci.org.au/thredds/ncss/zv2/agcd/v1/tmin/mean/r005/01day/agcd_v1_tmin_mean_r005_daily_",yrs[i],".nc?var=tmin&north=",latnorth[j],"&west=",lonwest[j],"&east=",loneast[j],"&south=",latsouth[j],"&horizStride=1&time_start=",yrs[i],"-01-01T09%3A00%3A00Z&time_end=",yrs[i],"-12-31T09%3A00%3A00Z&timeStride=100&addLatLon=true")
+  }
+}
+tmin.urls <- unlist(tmin.files)
 
 
 
-?list
+destpath <- "/Users/justinmathias/Library/CloudStorage/Dropbox/Research/UIdaho Postdoc/Tasmania/TasmaniaVegetation/Data/ClimateData/Daily/"
+
+var <- "precip"
+for (i in 1:length(ppt.urls)) {
+  download.file(url = ppt.urls[i], destfile = paste0(destpath), method = "wget")
+}
+
+download.file('https://www.wikipedia.org/', destfile = "mydirectory/wikipage.html", method = "wget", extra = "-r -p --random-wait")
+
+download.file(url = ppt.urls[1], destfile = paste0(destpath, "file.nc", method = "wget"))
+
+
+
+
+
+
+#I owe Livi 39.50.
+
+
 
 
 
--- RScripts/TasmaniaVegetation.R
+++ RScripts/TasmaniaVegetation.R
@@ -3,12 +3,6 @@
 library("easypackages")
 libraries(c("openxlsx", "tidyverse"))
 
-tco <- read.xlsx("/Users/justinmathias/Library/CloudStorage/Dropbox/Research/UIdaho Postdoc/Tasmania/TasmaniaVegetation/Data/Tassie_2023_TEC_Inventory.xlsx")
-tco
-unique(tco$Species)
-
-
-
-
-library("measurements")
-conv_unit(860, "metric_ton", "Mg")
+#Read in tec data
+tec <- read.xlsx("/Users/justinmathias/Library/CloudStorage/Dropbox/Research/UIdaho Postdoc/Tasmania/TasmaniaVegetation/Data/Tassie_2023_TEC_Inventory.xlsx", sheet = "final_data")
+unique(tec$FieldID)
